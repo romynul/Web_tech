f8dd663cc0bc7e1b7c9ac55389810de7a5d70429
Romynul Shuvo <rhshuvo97@gmail.com>
2018-10-01 12:45:09 +0600

    Add files via upload

---
+++ lab_3_php/index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<html>
+<head>
+</head>
+<body>
+<form name="" method="post" action="login.php">
+    <input type="text" id="name" name="name" placeholder="Your name..">
+    <input type="password" id="pass" name="pass" placeholder="Your Password..">
+    <input type="submit" value="Submit">
+
+</form>
+
+</body>
+</html>
---
+++ lab_3_php/login.php
@@ -0,0 +1,8 @@
+<?php
+
+$name= $_POST['name'];
+$pass=$_POST['pass'];
+
+echo "Your Name is : ".$name;
+echo "<br>Password is : ".$pass;
+?>
